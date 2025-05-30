a30c8199852fcf439ba90ca718792956ec94215c
liu-yangyi <108654654+liu-yangyi@users.noreply.github.com>
2025-05-30 22:12:07 +0800

    Delete data_stored_in_genome directory

--- data_stored_in_genome/genome_read.py
+++
@@ -1,8 +0,0 @@
-from Bio import SeqIO
-import gzip
-
-with gzip.open("mutated_genome.fsa.gz", "rt") as in_handle, open("mutated_genome.fasta", "w") as out_handle:
-    records = SeqIO.parse(in_handle, "fastq")
-    count = SeqIO.write(records, out_handle, "fasta")
-
-print(f"成功将 {count} 条记录写入 mutated_genome.fasta")
